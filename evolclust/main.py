08b101f1297c19788e980aecec797853430b1865
maks-ym <cadilac.bakhmut@gmail.com>
2018-11-20 22:53:44 +0100

    check preprocessed data in dataset

--- evolclust/main.py
+++ evolclust/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from .data import HAPT
-from .cluster import MeanMuAndLambda
+import data
+import cluster
 
 
 if __name__ == "__main__":
@@ -11,4 +11,16 @@ if __name__ == "__main__":
     # predict (test)
     # compare with k-means
     # visualize
-    pass
+
+    #read test
+    file_path = "data/hapt/train/X_train.txt"
+    train_data = []
+    with open(file_path) as in_file:
+        for line in in_file:
+            parsed_line = [float(x) for x in line.rstrip("\n").split(" ")]
+            train_data.append(parsed_line)
+    
+    for row in train_data:
+        print(len(row))
+    
+    print(len(train_data))
