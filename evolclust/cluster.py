ec0a0e84fcf9ab5ec5a5cc922d2cf256a276c313
Maks-ym <cadilac.bakhmut@gmail.com>
2018-12-05 14:40:49 +0100

    clustering alg & distance functions added with initial tests

--- evolclust/cluster.py
+++ evolclust/cluster.py
@@ -1,11 +1,47 @@
-# evolution algorithms live here
+# clustering functionality
 
-class MeanMuAndLambda:
-    def __init__(self):
-        self.model
-        
-    def train(self, data, labels):
-        return self.model
+from numpy.linalg import norm
+import numpy as np
 
-    def predict(self, data):
-        return labels
+
+class Distance:
+    @staticmethod
+    def manhattan(a, b):
+        a = np.array(a)
+        b = np.array(b)
+        return norm(a-b, ord=1)
+
+    @staticmethod
+    def euclidean(a, b):
+        a = np.array(a)
+        b = np.array(b)
+        return norm(a-b, ord=2)
+
+#TODO: tests
+class Centroids:
+    @staticmethod
+    def cluster(data, centers, dist_func="euclid"):
+        ''' dist_func=["euclid","manhat"] '''
+        dist_func = Distance.manhattan if dist_func == "manhat" else Distance.euclidean
+        samples_num = len(data)
+        dist_array = np.full(samples_num, float("inf"))
+        labels_array = np.zeros(samples_num)
+        for i in range(len(centers)):
+            for j in range(samples_num):
+                cur_dist = dist_func(centers[i],data[j])
+                if cur_dist < dist_array[j]:
+                    dist_array[j] = cur_dist
+                    labels_array[j] = i
+        return labels_array
+
+
+#TODO: tests
+class Evaluate:
+
+    @staticmethod
+    def informationGain():
+        pass
+
+    @staticmethod
+    def silhouette():
+        pass
---
+++ evolclust/test/test_cluster.py
@@ -0,0 +1,52 @@
+import cluster
+import numpy as np
+import os
+
+class TestDistance:
+    def test_manhattan_array(self):
+        a = [1,3,5,7,9]
+        b = [2,4,6,8,0]
+        c = [1,3,5,7,9]
+        d1 = cluster.Distance.manhattan(a, b)
+        assert d1 == 13
+        d2 = cluster.Distance.manhattan(a, c)
+        assert d2 == 0
+    
+    def test_euclidean_array(self):
+        a = [1,3,5,7,9]
+        b = [2,4,6,8,0]
+        c = [1,3,5,7,9]
+        d1 = cluster.Distance.euclidean(a, b)
+        assert str(d1)[:11] == "9.219544457"
+        d2 = cluster.Distance.euclidean(a, c)
+        assert d2 == 0
+
+
+class TestCentroids:
+    cur_path = os.path.dirname(os.path.abspath(__file__))
+    samples_path = os.path.join(cur_path, "test_data/x_test.txt")
+    labels_path = os.path.join(cur_path, "test_data/y_test.txt")
+    def test_centroids_cluster(self):
+        # read
+        samples = np.loadtxt(self.samples_path)
+        true_labels = np.loadtxt(self.labels_path)
+        # count clusters
+        unique = np.unique(true_labels, return_counts=False)
+        # create centroids
+        centers = []
+        for i in range(len(unique)):
+            cur_samples = samples[true_labels==unique[i]]
+            centers.append(np.mean(cur_samples, axis=0))
+        centers = np.array(centers)
+        # (manhattan dist)
+        labels_1 = cluster.Centroids.cluster(samples, centers) + 1
+        recall_1 = len(labels_1[labels_1==true_labels])/len(true_labels)
+        assert recall_1 > 0.8
+        # (euclidean dist)
+        labels_2 = cluster.Centroids.cluster(samples, centers) + 1
+        recall_2 = len(labels_2[labels_2==true_labels])/len(true_labels)
+        assert recall_2 > 0.8
+
+
+class TestEvaluate:
+    pass
